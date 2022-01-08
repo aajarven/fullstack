36d829855357a34cea6c0c02288e70ce6484987b
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2022-01-08 17:12:23 +0000

    ex 1.7

--- 01-react-basics/unicafe/src/App.js
+++ 01-react-basics/unicafe/src/App.js
@@ -4,8 +4,8 @@ const Button = ({handleClick, text}) => (
   <button onClick={handleClick}>{text}</button>
 )
 
-const Display = ({reviews, reviewType}) => (
-  <p>{reviewType}: {reviews[reviewType]}</p>
+const StatisticField = ({text, value}) => (
+  <p>{text}: {value}</p>
 )
 
 function App() {
@@ -28,6 +28,18 @@ function App() {
     }
   }
 
+  function sum(dict) {
+    return Object.values(dict).reduce((sum, next) => sum + next, 0)
+  }
+
+  function average(reviews) {
+    return (reviews.good - reviews.bad) / sum(reviews)
+  }
+
+  function positive_percent(reviews) {
+    return reviews.good / sum(reviews) * 100
+  }
+
   return (
     <>
       <h1>Give Feedback</h1>
@@ -36,9 +48,12 @@ function App() {
       <Button handleClick={addReview("bad")} text="bad" /> 
 
       <h1>Statistics</h1>
-      <Display reviews={reviews} reviewType="good" />
-      <Display reviews={reviews} reviewType="neutral" />
-      <Display reviews={reviews} reviewType="bad" />
+      <StatisticField text="good" value={reviews["good"]} />
+      <StatisticField text="neutral" value={reviews["neutral"]}  />
+      <StatisticField text="bad" value={reviews["bad"]}  />
+      <StatisticField text="total" value={sum(reviews)} />
+      <StatisticField text="average" value={average(reviews)} />
+      <StatisticField text="positive" value={positive_percent(reviews) + " %"} />
     </>
   );
 }
