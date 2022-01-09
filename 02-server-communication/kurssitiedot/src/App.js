52966c414ca83d2bc072edb7672db407570a864a
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2022-01-09 09:37:58 +0000

    ex 2.1

---
+++ 02-server-communication/kurssitiedot/src/App.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+const Header = ({course}) => (
+    <h1>{course.name}</h1>
+)
+
+const Part = ({part}) => (
+  <p>{part.name} {part.exercises}</p>
+)
+
+const Content = ({course}) => (
+  course.parts.map(part => <Part key={part.id} part={part} />)
+)
+
+const Total = (props) => {
+  let total = 0
+  for (let i=0; i < props.course.parts.length; i++) {
+    total += props.course.parts[i].exercises
+  }
+
+  return <p>Number of exercises {total}</p>
+}
+
+const Course = ({course}) => (
+  <div>
+    <Header course={course} />
+    <Content course={course} />
+    <Total course={course} />
+  </div>
+)
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Some difficult topic',
+        exercises: 99,
+        id: 4
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
